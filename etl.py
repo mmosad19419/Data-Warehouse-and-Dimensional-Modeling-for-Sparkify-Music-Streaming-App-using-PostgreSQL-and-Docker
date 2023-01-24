be735ebf744f9d4dd5dddbb33febf8137d10b7dd
Mohamed Mosad <mmosad19419@gmail.com>
2023-01-25 00:22:05 +0200

    update files

--- etl.py
+++ etl.py
@@ -11,7 +11,7 @@ def get_files(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
         for f in files :
             all_files.append(os.path.abspath(f))
-    
+
     return all_files
 
 
@@ -26,21 +26,21 @@ def process_song_file(cur, filepath):
     artist_name = df.iloc[0]["artist_name"]
     year = int(df.iloc[0]["year"])
     duration = float(df.iloc[0]["duration"])
-    
-    song_data = list((song_id, title, artist_id, artist_name, year, duration))
 
+    song_data = list((song_id, title, artist_id, artist_name, year, duration))
     cur.execute(song_table_insert, song_data)
 
-        
+
+
     # insert artist record
     artist_id = df.iloc[0]["artist_id"]
     artist_name = df.iloc[0]["artist_name"]
     artist_location = df.iloc[0]["artist_location"]
     artist_latitude = df.iloc[0]["artist_latitude"]
     artist_longitude = df.iloc[0]["artist_longitude"]
-    
+
     artist_data = list((artist_id, artist_name, artist_location, artist_latitude, artist_longitude))
-    
+
     cur.execute(artist_table_insert, artist_data)
 
 def process_log_file(cur, filepath):
@@ -52,50 +52,49 @@ def process_log_file(cur, filepath):
 
     # convert timestamp column to datetime
     t = df.iloc[0]['ts']
-    
+
     # insert time data records
     time_data = pd.Series(pd.to_datetime(t, unit = "ms"), dtype = "datetime64[ns]")
-    
-    df_dict = {"session_id" : df.iloc[0]["sessionId"],
-               "start_time" : time_data.dt.time,
+
+    df_dict = {"start_time" : int(t),
                "hour" : time_data.dt.hour,
                "day" : time_data.dt.day,
                "week" : time_data.dt.week,
-               "month" : time_data.dt.week, 
-               "year" : time_data.dt.year, 
+               "month" : time_data.dt.week,
+               "year" : time_data.dt.year,
                "weekday" : time_data.dt.weekday}
-    
+
     time_df = pd.DataFrame(df_dict)
-    
+
     for i, row in time_df.iterrows():
-        
+
         cur.execute(time_table_insert, list(row))
-    
+
 
     # load user table
     user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
-    for i, row in user_df.iterrows():
+    for index, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
-        
+
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
             songid, artistid = None, None
-    
+
         start_time = time_data.dt.time[0]
-        
+
         # insert songplay record
-        songplay_data = tuple([start_time, row.sessionId, row.userId, songid, artistid, row.level, row.location, row.userAgent])
+        songplay_data = tuple([int(t), row.sessionId, row.userId, songid, artistid, row.level, row.location, row.userAgent])
         cur.execute(songplay_table_insert, songplay_data)
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -8,14 +8,17 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-user_table_create = (""" CREATE TABLE IF NOT EXISTS users
+user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users
                             (user_id INT PRIMARY KEY UNIQUE NOT NULL,
-                            first_name VARCHAR,
-                            last_name VARCHAR,
-                            gender CHAR, level VARCHAR);
+                            first_name VARCHAR NOT NULL,
+                            last_name VARCHAR NOT NULL,
+                            gender CHAR,
+                            level VARCHAR NOT NULL);
 """)
 
-song_table_create = (""" CREATE TABLE IF NOT EXISTS songs
+song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs
                     (song_id VARCHAR PRIMARY KEY UNIQUE NOT NULL,
                     title VARCHAR NOT NULL, artist_id VARCHAR NOT NULL,
                     artist_name VARCHAR NOT NULL,
@@ -23,7 +26,8 @@ song_table_create = (""" CREATE TABLE IF NOT EXISTS songs
                     duration FLOAT8 NOT NULL);
 """)
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
+artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists
                       (artist_id VARCHAR PRIMARY KEY UNIQUE NOT NULL,
                       name VARCHAR NOT NULL,
                       location VARCHAR,
@@ -31,84 +35,117 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
                       longitude FLOAT8);
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time
-                    (session_id VARCHAR NOT NULL,
-                    start_time TIME NOT NULL,
-                    hour VARCHAR, day VARCHAR,
-                    week VARCHAR, month VARCHAR,
-                    year VARCHAR,
-                    weekday VARCHAR,
-                    PRIMARY KEY (start_time, session_id));
+time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time
+                    (start_time BIGINT PRIMARY KEY NOT NULL,
+                    hour VARCHAR NOT NULL,
+                    day VARCHAR NOT NULL,
+                    week VARCHAR NOT NULL,
+                    month VARCHAR NOT NULL,
+                    year VARCHAR NOT NULL,
+                    weekday VARCHAR NOT NULL);
 """)
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
-                        (songplay_id SERIAL PRIMARY KEY NOT NULL,
-                        start_time TIME NOT NULL,
-                        session_id VARCHAR UNIQUE NOT NULL,
-                        user_id INT UNIQUE NOT NULL,
-                        song_id VARCHAR UNIQUE,
-                        artist_id VARCHAR UNIQUE,
-                        level VARCHAR,
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays
+                        (songplay_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+                        start_time BIGINT NOT NULL,
+                        session_id VARCHAR NOT NULL,
+                        user_id INT NOT NULL,
+                        song_id VARCHAR,
+                        artist_id VARCHAR,
+                        level VARCHAR NOT NULL,
                         location VARCHAR,
                         user_agent VARCHAR);
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays
-                            (start_time, session_id, user_id, song_id, artist_id, level, location, user_agent)
-                        VALUES
-                                (%s, %s, %s, %s, %s, %s, %s, %s)
-                        ON CONFLICT (user_id)
-                        DO UPDATE level = %s;
+songplay_table_insert = ("""
+INSERT INTO songplays
+    (start_time,
+    session_id,
+    user_id,
+    song_id,
+    artist_id,
+    level,
+    location,
+    user_agent)
+VALUES
+        (%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 
 # user table insertion
-user_table_insert = ("""INSERT INTO users
-                        (user_id, first_name, last_name, gender, level)
-                    VALUES
-                        (%s, %s, %s, %s, %s)
-                    ON CONFLICT (user_id)
-                    DO UPDATE level = %s;
+user_table_insert = ("""
+INSERT INTO users
+    (user_id,
+    first_name,
+    last_name,
+    gender,
+    level)
+VALUES
+    (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO UPDATE SET level = excluded.level;
 """)
 
 
 # song table insertion
-song_table_insert = ("""INSERT INTO songs
-                        (song_id, title, artist_id, artist_name, year, duration)
-                    VALUES
-                        (%s, %s, %s, %s, %s, %s)
-                    ON CONFLICT (song_id)
-                    DO UPDATE duration = %s;
+song_table_insert = ("""
+INSERT INTO songs
+    (song_id,
+    title,
+    artist_id,
+    artist_name,
+    year,
+    duration)
+VALUES
+    (%s, %s, %s, %s, %s, %s)
+ON CONFLICT (song_id)
+DO UPDATE SET duration = excluded.duration;
 """)
 
 
 # song table insertion
-artist_table_insert = ("""INSERT INTO artists
-                          (artist_id, name, location, latitude, longitude)
-                    VALUES
-                          (%s, %s, %s, %s, %s)
-                    ON CONFLICT (artist_id)
-                    DO UPDATE name = %s;
+artist_table_insert = ("""
+INSERT INTO artists
+      (artist_id,
+      name,
+      location,
+      latitude,
+      longitude)
+VALUES
+      (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id)
+DO UPDATE SEt name = excluded.name;
 """)
 
 
 # time table insert
-time_table_insert = ("""INSERT INTO time
-                        (session_id, start_time, hour, day, week, month, year, weekday)
-                    VALUES
-                        (%s, %s, %s, %s, %s, %s, %s, %s)
-                    ON CONFLICT (artist_id)
-                    DO UPDATE start_time = %s;
+time_table_insert = ("""
+INSERT INTO time
+    (start_time,
+    hour,
+    day,
+    week,
+    month,
+    year,
+    weekday)
+VALUES
+    (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
 
-song_select = ("""SELECT song_id, artist_id FROM songs
-                  WHERE title LIKE %s
-                  AND artist_name LIKE %s
-                  AND duration = %s;
+song_select = ("""
+SELECT song_id, artist_id
+FROM songs
+WHERE title LIKE %s
+AND artist_name LIKE %s
+AND duration = %s
+GROUP BY song_id,
+         artist_id;
 """)
 
 # QUERY LISTS
